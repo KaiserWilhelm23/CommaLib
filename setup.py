af55bf846b24f10ff8de2998b7700bc08b1b59a5
Will Payne <67875237+blaze005@users.noreply.github.com>
2021-02-13 17:09:17 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="CommaLib", 
+    version="1.0.0",
+    author="Will Payne",
+    author_email="willhpayne@icloud.com",
+    description="Puts commas in big numbers for you.",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5',
+)
